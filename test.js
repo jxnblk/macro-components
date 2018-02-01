465b5b1ef110a0f43d6e17720fd0d106a87eaadf
Brent Jackson <jxnblk@gmail.com>
2018-01-31 19:37:41 -0500

    Add support for using components without a displayName

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ import macro, { Clone } from './src'
 const Box = props => <div {...props} />
 const Text = props => <div {...props} />
 const Heading = props => <h2 {...props} />
+const NoName = props => <pre {...props} />
 
 test('returns a component', t => {
   const Card = macro(({ h1, div }) => (
@@ -186,5 +187,23 @@ test('Clone returns false with no element', t => {
     />
   ).toJSON()
   t.is(json, null)
+})
+
+test('element function can use component type as keys', t => {
+  const Card = macro(elements => (
+    <div>
+      {elements[Box]}
+      {elements[NoName]}
+    </div>
+  ))
+  const json = render(
+    <Card>
+      <Box>Hello</Box>
+      <NoName>Hi</NoName>
+    </Card>
+  ).toJSON()
+  t.is(json.children[0].children[0], 'Hello')
+  t.is(json.children[1].type, 'pre')
+  t.is(json.children[1].children[0], 'Hi')
 })
 
